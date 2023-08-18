b4696eff76223cc3bdf6ea3737c2f45eb13b48d8
ferhadeyvazov <eyvazov.ferhad1997@gmail.com>
2023-08-18 17:44:19 +0400

    Button onclick func was added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,36 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import css from './Product.module.css';
 
 function Product() {
-  const products = useSelector((state) => state.allProducts.products);
-  
-  let renderList = products.map((product)=>{
-    const {id, title, image, price, category} = product;
-    return (
-      <Link to={`/product/${id}`} key={id}>
-          <div className={css.card} >
-            <div className={css.card__image}>
-              <img src={image} alt={"cardImage"+id} />
-            </div>
-            <div className={css.card__content}>
-              <h3 className={css.title}>{title}</h3>
-              <h4 className={css.price}>$ {price}</h4>
-              <p className={css.category}>{category}</p>
-              <button className={css.addCartBtn}>Add to Cart</button>
-            </div>
-          </div>
+  const [basket, setBasket] = useState([]);
+  function addArr(product) {
+      const arr = [...basket];
+      console.log(product);
+      arr.map(item=>{
+        if(product.id != item.id) {
+          setBasket(arr(product));
+          console.log(arr);
+        }
+      })
 
-      </Link>
+    }
+  
+  const products = useSelector((state) => state.allProducts.products);
+
+  let renderList = products.map((product, index) => {
+    const { id, title, image, price, category } = product;
+    return (
+      <div className={css.card} key={id}>
+        <div className={css.card__image}>
+          <Link to={`/product/${id}`} >
+            <img src={image} alt={"cardImage" + id} />
+          </Link>
+        </div>
+        <div className={css.card__content}>
+          <h3 className={css.title}>{title}</h3>
+          <h4 className={css.price}>$ {price}</h4>
+          <p className={css.category}>{category}</p>
+          <button
+          onClick={()=>addArr(product)}
+          className={css.addCartBtn}>Add to Cart</button>
+        </div>
+      </div>
+
     );
   })
 
-  return(
-      <div className={css.product__cards}>
-        
+  return (
+    <div className={css.product__cards}>
+
       {renderList}
-      </div>
+    </div>
   )
 }
 export default Product
